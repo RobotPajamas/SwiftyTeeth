a6fdc1a686860b75d00e1b26c901f7825b3cc04d
SJ <suresh@vicarasolutions.com>
2019-11-24 00:15:11 -0500

    See issue #36: Moved SwiftyTeeth to a separate directory, and added a SwiftyTooth directory

--- Package.swift
+++ Package.swift
@@ -32,6 +32,9 @@ let package = Package(
         .library(
             name: "SwiftyTeeth",
             targets: ["SwiftyTeeth"]),
+        .library(
+            name: "SwiftyTooth",
+            targets: ["SwiftyTooth"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -41,8 +44,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftyTeeth",
-            path: "Sources"),
+            name: "SwiftyTeeth"),
+        .target(
+            name: "SwiftyTooth"),
 //        .testTarget(
 //            name: "SwiftyTeeth Tests",
 //            dependencies: ["SwiftyTeeth"]),
---
+++ Sources/SwiftyTooth/SwiftyTooth.swift
@@ -0,0 +1,98 @@
+//
+//  SwiftyTooth.swift
+//  SwiftyTeeth
+//
+//  Created by Suresh Joshi on 2019-11-23.
+//
+
+import Foundation
+import CoreBluetooth
+
+var swiftyToothLogger: Logger?
+
+open class SwiftyTooth: NSObject {
+
+    public static let shared = SwiftyTooth()
+    
+    public var stateChangedHandler: ((BluetoothState) -> Void)? {
+        didSet {
+            stateChangedHandler?(state)
+        }
+    }
+
+    open lazy var peripheralManager: CBPeripheralManager = {
+        let instance = CBPeripheralManager(
+            delegate: self,
+            queue: DispatchQueue(label: "com.robotpajamas.SwiftyTooth"))
+        // Throwaway command to init CoreBluetooth (helps prevent timing problems)
+        return instance
+    }()
+
+    public var state: BluetoothState {
+        return BluetoothState(rawValue: peripheralManager.state.rawValue) ?? .unknown
+    }
+    
+    public override init() {
+    }
+}
+
+public extension SwiftyTooth {
+    class var logger: Logger? {
+        get {
+            return swiftyToothLogger
+        }
+        set {
+            swiftyToothLogger = newValue
+        }
+    }
+}
+
+// MARK: - Manager Advertise functions
+public extension SwiftyTooth {
+
+    var isAdvertising: Bool {
+        return peripheralManager.isAdvertising
+    }
+    
+    func advertise(name: String, for timeout: TimeInterval = 0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + timeout) {
+            self.stopAdvertising()
+        }
+        advertise(name: name)
+    }
+    
+    func advertise(name: String) {
+        peripheralManager.startAdvertising([CBAdvertisementDataLocalNameKey:name])
+    }
+    
+    func stopAdvertising() {
+        peripheralManager.stopAdvertising()
+    }
+}
+
+// MARK: - Peripheral manager
+extension SwiftyTooth: CBPeripheralManagerDelegate {
+    public func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        switch (peripheral.state) {
+        case .unknown:
+            Log(v: "Bluetooth state is unknown.")
+        case .resetting:
+            Log(v: "Bluetooth state is resetting.")
+        case .unsupported:
+            Log(v: "Bluetooth state is unsupported.")
+        case .unauthorized:
+            Log(v: "Bluetooth state is unauthorized.")
+        case .poweredOff:
+            Log(v: "Bluetooth state is powered off.")
+        case .poweredOn:
+            Log(v: "Bluetooth state is powered on")
+        default:
+            Log(v: "Bluetooth state is not in supported switches")
+        }
+        
+        guard let state = BluetoothState(rawValue: peripheral.state.rawValue) else {
+            return
+        }
+        stateChangedHandler?(state)
+    }
+}
