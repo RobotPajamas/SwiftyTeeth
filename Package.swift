a8ae2b99f2dc13eb7efa17046580100541522f5b
SJ <suresh@vicarasolutions.com>
2019-11-23 22:38:41 -0500

    See issue #33: Fixing another dependency ordering problem

--- Package.swift
+++ Package.swift
@@ -27,15 +27,15 @@ let package = Package(
         .tvOS(.v10),
         .watchOS(.v4)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyTeeth",
             targets: ["SwiftyTeeth"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
