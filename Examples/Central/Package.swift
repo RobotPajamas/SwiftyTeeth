967828b8e520d17367f629790dbc5e5c1676809a
SJ <suresh@vicarasolutions.com>
2020-03-27 22:32:58 -0400

    Unrelated to SwiftUI, just want to make sure I don't forget to create this small sample

---
+++ Examples/Central/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Central",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(name: "SwiftyTeeth", path: "../../"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Central",
+            dependencies: ["SwiftyTeeth"]),
+    ]
+)
