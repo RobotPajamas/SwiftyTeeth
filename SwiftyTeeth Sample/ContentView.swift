0de292e936ec790a756bc30f4e9b0a7e4535bee3
SJ <suresh@vicarasolutions.com>
2020-03-27 22:45:39 -0400

    See issue #41: Removed old Storyboard code, and created new SwiftUI project

---
+++ SwiftyTeeth Sample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftyTeeth Sample
+//
+//  Created by SJ on 2020-03-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
