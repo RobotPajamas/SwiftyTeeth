3609cffa3bfdd4f98cbce259ed6cd4e885ad7363
sj <suresh@vicarasolutions.com>
2023-07-02 10:46:20 -0400

    See issue #58: Updating versioning

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // Copyright (c) 2020 Robot Pajamas
@@ -22,10 +22,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftyTeeth",
     platforms: [
-        .macOS(.v10_14),
-        .iOS(.v10),
-        .tvOS(.v10),
-        .watchOS(.v4)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
