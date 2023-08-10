912d60b7a19b039fb97bdab86df175b99eb379ce
sj <suresh@vicarasolutions.com>
2023-08-10 03:36:06 -0400

    See issue #54: Adding files to Xcode

--- Package.swift
+++ Package.swift
@@ -27,7 +27,6 @@ let package = Package(
         .watchOS(.v6),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyTeeth",
             targets: ["SwiftyTeeth"]),
